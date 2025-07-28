9aa6a116e414ae62557baaadc9ac19d6f400d828
Alexander-Weinhart <alex@onlinedesignsbyalex.com>
2025-07-28 11:35:30 -0400

    git push ID #10

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-     $("#navbar-plh").load("/html-elem/navbar.html", function() {
+     $("#navbar-plh").load("html-elem/navbar.html", function() {
          console.log("Navbar loaded successfully");
          document.dispatchEvent(new Event("navbarLoaded"));
      });
-    $("#footer-plh").load("/html-elem/footer.html", function() {
+    $("#footer-plh").load("html-elem/footer.html", function() {
         console.log("Footer loaded successfully");
         document.dispatchEvent(new Event("footerLoaded"));
     });
